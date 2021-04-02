2cd8e52ffa62b00d8d3bbb20af0ca5382a2dac20
VioletViola <vivian.v18723@gmail.com>
2021-04-02 04:25:42 +0100

    Add overlay for navbar on small screens.

--- scripts.js
+++ scripts.js
@@ -2,6 +2,13 @@ $("document").ready(function () {
   // toggles small nav
   $("#barsBg .fas.fa-bars").on("click", () => {
     $("#smallNavBg").slideToggle();
+    $(".overlay").fadeToggle("fast");
+  });
+
+  // close small nav from overlay
+  $(".overlay").on("click", () => {
+    $("#smallNavBg").slideToggle();
+    $(".overlay").css("display", "none");
   });
 
   // shows socials
